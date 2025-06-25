14785ec5d9f3b0e00e217b81801d371deedd43cd
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-06-26 00:25:23 +0300

    Connected to the server

---
+++ connection/connection.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "cinema_db";
+
+$mysqli = new mysqli($host, $username, $password, $database);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
