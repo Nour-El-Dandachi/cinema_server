fd64d452546e98037625e6848647873011dd049f
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-06-29 16:41:52 +0300

    Updated showtimes

--- models/Showtime.php
+++ models/Showtime.php
@@ -7,7 +7,8 @@ class Showtime extends Model {
     private int $id;
     private int $movie_id;
     private int $auditorium_id;
-    private string $start_time;
+    private string $day;
+    private string $time;
     private string $language;
     private int $subtitled;
     private string $created_at;
@@ -18,7 +19,8 @@ class Showtime extends Model {
         $this->id = $data["id"];
         $this->movie_id = $data["movie_id"];
         $this->auditorium_id = $data["auditorium_id"];
-        $this->start_time = $data["start_time"];
+        $this->day = $data["day"];
+        $this->time = $data["time"];
         $this->language = $data["language"];
         $this->subtitled = $data["subtitled"];
         $this->created_at = $data["created_at"];
@@ -27,14 +29,16 @@ class Showtime extends Model {
     public function getId(): int { return $this->id; }
     public function getMovieId(): int { return $this->movie_id; }
     public function getAuditoriumId(): int { return $this->auditorium_id; }
-    public function getStartTime(): string { return $this->start_time; }
+    public function getDay(): string { return $this->day; }
+    public function getTime(): string { return $this->time; }
     public function getLanguage(): string { return $this->language; }
     public function isSubtitled(): int { return $this->subtitled; }
     public function getCreatedAt(): string { return $this->created_at; }
 
     public function setMovieId(int $movie_id) { $this->movie_id = $movie_id; }
     public function setAuditoriumId(int $auditorium_id) { $this->auditorium_id = $auditorium_id; }
-    public function setStartTime(string $start_time) { $this->start_time = $start_time; }
+    public function setDay(string $day) { $this->day = $day; }
+    public function setTime(string $time) { $this->time = $time; }
     public function setLanguage(?string $language) { $this->language = $language; }
     public function setSubtitled(int $subtitled) { $this->subtitled = $subtitled; }
 
@@ -43,7 +47,8 @@ class Showtime extends Model {
             "id" => $this->id,
             "movie_id" => $this->movie_id,
             "auditorium_id" => $this->auditorium_id,
-            "start_time" => $this->start_time,
+            "day" => $this->day,
+            "time" => $this->time,
             "language" => $this->language,
             "subtitled" => $this->subtitled,
             "created_at" => $this->created_at
@@ -51,11 +56,11 @@ class Showtime extends Model {
     }
 
     public static function findByDate(mysqli $mysqli, string $date): ?array {
-        $sql = "SELECT * FROM showtimes WHERE DATE(start_time) = ?";
+        $sql = "SELECT * FROM showtimes WHERE day = ?";
 
         $query = $mysqli->prepare($sql);
         $query->bind_param("s", $date);
-        
+
         $query->execute();
         $result = $query->get_result();
 
@@ -66,6 +71,4 @@ class Showtime extends Model {
 
         return $showtimes;
     }
-
-
 }
